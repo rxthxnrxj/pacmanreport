6a51501d136cec191287bb639578b89b51af13e4
unknown <rxthxnrxj@gmail.com>
2023-11-19 11:19:53 -0800

    added team dynamics and emojis

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ const App = () => {
   const [foodOneExist, setFoodOneExist] = useState(true)
   const [foodTwoExist, setFoodTwoExist] = useState(true)
   const [foodThreeExist, setFoodThreeExist] = useState(true)
+  const [teamDynamic, setTeamDynamic] = useState(false)
+  const [boardView, setBoardView] = useState(true)
 
   useEffect(() => {
     const handleKeyPress = (e) => {
@@ -93,6 +95,15 @@ const App = () => {
       setChallengeTwo(true)
     }
     
+    if(!foodOneExist & !foodTwoExist & !foodThreeExist & !challengeOneExists & !challengeTwoExists){
+      setTeamDynamic(true);
+      setFoodTwo(false)
+      setFoodOne(false)
+      setFoodThree(false)
+      setChallengeOne(false)
+      setChallengeTwo(false)
+      setBoardView(false)
+    }
 
   },[pacManPosition])
 
@@ -103,37 +114,44 @@ const App = () => {
       <Row className='mt-3'>
       {foodOne && (<Row>
         <Col xl={4} className='card'>
-          <h5 className='cardTitle'>Week 5</h5>
-          <p className='cardText m-0'>Implemented DFS, BFS, UCS and A* to help Pac-Man find a piece of food, just like you did now *wink*</p>
+          <h5 className='cardTitle'><i class="fa-solid fa-calendar-days"></i> Week 5</h5>
+          <p className='cardText m-0'>Implemented DFS, BFS, UCS and A* to help Pac-Man find a piece of food <i class="fa-solid fa-utensils"></i> - just like you did now <i class="fa-solid fa-face-grin-wink"></i></p>
+        </Col>
+      </Row>)}
+      {teamDynamic && (<Row>
+        <Col xl={4} className='cardTeamDynamic'>
+          <h5 className='cardTitle'><i class="fa-solid fa-people-group"></i> Team Dynamic</h5>
+          <p className='cardText m-0' style={{color:'#f7f7f7'}}><i class="fa-solid fa-hand-fist"></i> Strong, we would say <i class="fa-solid fa-face-grin-wide"></i> <br/> <i class="fa-solid fa-ellipsis"></i><br/>We had difference in opinions when discussing or computing the various optimal paths. However, with effective team collaboration, open discussions and after a lot of brainstorming sessions we succeed in overcoming these challenges<br/> <i class="fa-solid fa-ellipsis"></i> <br/> We even made a friendly bet on who could find the most optimal solution (which obviously the 'PacMan' won) <br/> <i class="fa-solid fa-sack-dollar fa-bounce"></i></p>
         </Col>
       </Row>)}
       {foodTwo && (<Row>
         <Col xl={4} className='card'>
-          <h5 className='cardTitle'>Week 6</h5>
+          <h5 className='cardTitle'><i class="fa-solid fa-calendar-days"></i> Week 6</h5>
           <p className='cardText m-0'>Implemented BFS, a non-trivial heuristic for A* to find all CORNERS in the game</p>
         </Col>
       </Row>)}
       {foodThree && (<Row>
         <Col xl={4} className='card'>
-        <h5 className='cardTitle'>Week 7</h5>
+        <h5 className='cardTitle'><i class="fa-solid fa-calendar-days"></i> Week 7</h5>
         <p className='cardText m-0'>Implemented A* search in order to eat all the dots on map</p>
         </Col>
       </Row>)}
       {challengeOne && (<Row>
         <Col xl={4} className='cardChallenge'>
-        <h5 className='cardTitle'>A* search algorithm</h5>
-        <p className='cardTextChallenge m-0'>Succeeded in implementing A* search algorithm with a non-trivial corners' heuristic and gained a deeper understanding about the fringe and improved the efficiency</p>
+        <h5 className='cardTitle'><i class="fa-solid fa-star"></i> A* search algorithm <i class="fa-solid fa-star"></i></h5>
+        <p className='cardTextChallenge m-0'>Succeeded <i class="fa-solid fa-champagne-glasses"></i> in implementing A* search algorithm with a non-trivial corners' heuristic and gained a deeper understanding about the fringe and improved the efficiency</p>
         </Col>
       </Row>)}
       {challengeTwo && (<Row>
-        <Col xl={4} className='cardChallenge'><h5 className='cardTitle'>Expanded Nodes - Great (TA Comment)</h5>
-        <p className='cardTextChallenge m-0'>For both BFS and Corners' Problem: Heuristic with A* were great - TA quote, we state </p></Col>
+        <Col xl={4} className='cardChallenge'><h5 className='cardTitle'><i class="fa-solid fa-star"></i> Expanded Nodes - Great (TA Comment) <i class="fa-solid fa-star"></i></h5>
+        <p className='cardTextChallenge m-0'>For both BFS and Corners' Problem: Heuristic with A* were great <i class="fa-solid fa-champagne-glasses"></i> - TA quote, we state </p></Col>
         
       </Row>)}
+
       </Row>
       
-      
-      <Row>
+      {boardView && (
+        <Row>
         <Col className="game-board"><PacMan position={pacManPosition} />
       {foodOneExist && (<Food position={{ x: 100, y: 100 }}/>)}
       {foodTwoExist && (<Food position={{ x: 230, y: 320 }}/>)}
@@ -142,6 +160,8 @@ const App = () => {
       {challengeTwoExists && (<Challenge position={{ x: 310, y: 30 }}/>)}
       </Col>
       </Row>
+      )}
+      
 
       <div style={{marginTop:'1rem'}}>
       <p className='cardTextChallenge m-0'>By PacMan and PacWoman</p>
